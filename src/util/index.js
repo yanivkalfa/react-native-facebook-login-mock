0417b2bd2d5b8368d50a909ff0effea220451f3e
yanivkalfa <yanivkalfa@yanivkalfas-Mac.local>
2016-03-11 11:10:44 -0500

    change P for tiny-promisify and change version

--- src/util/index.js
+++ src/util/index.js
@@ -1,40 +1,22 @@
-import P from 'bluebird';
+import Promisify from 'tiny-promisify';
 
 import { NativeModules } from 'react-native';
 const { FBLoginManager } = NativeModules;
 
-function createParams(params = [], resolve, reject){
-  params.push((err, data) => {
-    if (!err) {
-      return resolve(data);
-    } else {
-      err = (err && err instanceof Error) ? err : new Error(err);
-      return reject(err);
-    }
-  });
-  return params;
-}
-
-function createPromise(fn){
-  return new P((resolve, reject) => {
-    fn.apply(this, createParams(Array.prototype.splice.call(arguments, 1), resolve, reject ));
-  });
-}
-
 export async function loginWithPermissions(permissions = []) {
-  return await createPromise(FBLoginManager.loginWithPermissions, permissions);
+  return Promisify(FBLoginManager.loginWithPermissions)(permissions);
 }
 
 export async function loginWithoutPermissions() {
-  return await createPromise(FBLoginManager.login);
+  return Promisify(FBLoginManager.login)();
 }
 
 export async function getFBCredentials() {
-  return await createPromise(FBLoginManager.getCredentials);
+  return Promisify(FBLoginManager.getCredentials)();
 }
 
 export async function logout() {
-  return await createPromise(FBLoginManager.logout);
+  return Promisify(FBLoginManager.logout)();
 }
 
 export async function login(permissions = false) {
