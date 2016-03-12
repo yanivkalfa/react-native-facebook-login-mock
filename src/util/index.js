98dd3574e5c9e818f65054b602b03773ae84856e
yanivkalfa <yanivkalfa@yanivkalfas-Mac.local>
2016-03-12 18:19:43 -0500

    optimized promises and changed versions

--- src/util/index.js
+++ src/util/index.js
@@ -1,24 +1,28 @@
-import Promisify from 'tiny-promisify';
-
+import promisify from 'tiny-promisify';
 import { NativeModules } from 'react-native';
 const { FBLoginManager } = NativeModules;
 
+const getCredentialsAsync = promisify(FBLoginManager.getCredentials);
+const loginWithPermissionsAsync = promisify(FBLoginManager.loginWithPermissions);
+const loginAsync = promisify(FBLoginManager.login);
+const logoutAsync = promisify(FBLoginManager.logout);
+
+export async function getFBCredentials() {
+  return await getCredentialsAsync();
+}
+
 export async function loginWithPermissions(permissions = []) {
-  return Promisify(FBLoginManager.loginWithPermissions)(permissions);
+  return await loginWithPermissionsAsync(permissions);
 }
 
 export async function loginWithoutPermissions() {
-  return Promisify(FBLoginManager.login)();
-}
-
-export async function getFBCredentials() {
-  return Promisify(FBLoginManager.getCredentials)();
+  return await loginAsync();
 }
 
 export async function logout() {
-  return Promisify(FBLoginManager.logout)();
+  return await logoutAsync();
 }
 
 export async function login(permissions = false) {
-  return  (permissions) ? await loginWithPermissions(permissions) : await loginWithoutPermissions();
-}
+  return (permissions) ? await loginWithPermissions(permissions) : await loginWithoutPermissions();
+}   
